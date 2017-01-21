32b14961bd3eae9c5e94f51b2a63e9c01a1fcb10
Jimmy Zeng <jimmy@nufitmedia.com>
2017-01-21 16:16:19 -0500

    rename static page routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,12 @@ Rails.application.routes.draw do
   get 'stories/create'
 
   root 'static_pages#home'
-  get 'static_pages/home'
 
-  get 'static_pages/about'
+  get 'about', to: 'static_pages#about'
 
-  get 'static_pages/interview_guide'
+  get 'interview_guide', to: 'static_pages#interview_guide'
 
-  get 'static_pages/submit'
-
-  get 'static_pages/contact'
+  get 'contact', to: 'static_pages#contact'
 
 #Casein routes
 namespace :casein do
