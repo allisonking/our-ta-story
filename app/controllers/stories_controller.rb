d47b52f978ab17bafc88f4d85027d9499e922d6b
commanderking <jek582@mail.harvard.edu>
2017-01-21 14:45:26 -0500

    add form for submitting stories, index displays stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,14 +1,32 @@
 class StoriesController < ApplicationController
   def index
-    @rails = "Rails sucks!"
+    @stories = Story.all
   end
 
   def new
+    @story = Story.new
   end
 
   def show
+    @casein_page_title = 'View story'
+    @story = Story.find params[:id]
   end
 
   def create
+    @story = Story.new story_params
+
+    if @story.save
+      flash[:notice] = 'Story created'
+      redirect_to stories_path
+    else
+      flash.now[:warning] = 'There were problems when trying to create a new story'
+      render action: :new
+    end
   end
+  private
+
+  def story_params
+    params.require(:story).permit(:name, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,7 @@ namespace :casein do
     resources :stories
 end
 
+resources :stories
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
