079bf4112b36a60194a6d43371e35af7606bfd20
Allison King <allisonjuliaking@gmail.com>
2017-01-21 12:31:16 -0500

    adding static page controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def interview_guide
+  end
+
+  def submit
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/interview_guide'
+
+  get 'static_pages/submit'
+
+  get 'static_pages/contact'
 
 #Casein routes
 namespace :casein do
