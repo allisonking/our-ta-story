92159a3bf7d872e6aa3698969e151866240cb637
Jimmy Zeng <jimmy@nufitmedia.com>
2017-01-21 23:07:40 -0500

    fix /casein and disable imagemagick

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -2,7 +2,7 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
@@ -10,9 +10,9 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
@@ -22,21 +22,21 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
      "/assets/" + [version_name, "default.png"].compact.join('_')
    end
 
-  version :large do
-    process resize_to_limit: [800, 800]
-  end
- 
-  version :medium, :from_version => :large do
-    process resize_to_limit: [500, 500]
-  end
-
-  version :thumb, :from_version => :medium do
-    process resize_to_fit: [100, 100]
-  end
-
-  version :square do
-    process :resize_to_fill => [500, 500]
-  end
+  # version :large do
+  #   process resize_to_limit: [800, 800]
+  # end
+  #
+  # version :medium, :from_version => :large do
+  #   process resize_to_limit: [500, 500]
+  # end
+  #
+  # version :thumb, :from_version => :medium do
+  #   process resize_to_fit: [100, 100]
+  # end
+  #
+  # version :square do
+  #   process :resize_to_fill => [500, 500]
+  # end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,9 +1,10 @@
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_KEY'],
       :aws_secret_access_key  => ENV['AWS_SECRET'],
-      :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
+      :region                 => 'us-west-2' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV['AWS_BUCKET']
 end
