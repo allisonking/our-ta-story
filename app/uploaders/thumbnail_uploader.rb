789979933d931f88036db806eac4a1972ac334a5
Allison King <allisonjuliaking@gmail.com>
2017-01-21 22:40:36 -0500

    adding fog and mini magick

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -2,11 +2,11 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  #storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -21,6 +21,22 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   #
      "/assets/" + [version_name, "default.png"].compact.join('_')
    end
+
+  version :large do
+    process resize_to_limit: [800, 800]
+  end
+ 
+  version :medium, :from_version => :large do
+    process resize_to_limit: [500, 500]
+  end
+
+  version :thumb, :from_version => :medium do
+    process resize_to_fit: [100, 100]
+  end
+
+  version :square do
+    process :resize_to_fill => [500, 500]
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
