3d9020a193f6ba6a2afe46c8200d3d5708783439
Jimmy Zeng <jimmy@nufitmedia.com>
2017-01-22 09:56:25 -0500

    use real image for featured article and add submit stories button on home

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -16,9 +16,9 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+
      "/assets/" + [version_name, "default.png"].compact.join('_')
    end
 
