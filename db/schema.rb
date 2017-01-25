eb715ff83caa3a373fcf1cac1bd009f42e44885f
Allison King <allisonjuliaking@gmail.com>
2017-01-24 20:34:03 -0500

    adds ranking to db and casein

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122215301) do
+ActiveRecord::Schema.define(version: 20170124235804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170122215301) do
     t.string   "thumbnail"
     t.string   "media_type"
     t.string   "video_link"
+    t.integer  "rank"
   end
 
 end
