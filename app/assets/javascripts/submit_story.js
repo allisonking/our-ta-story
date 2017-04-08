0c87a35954f40df6c12ff7612bda5a3abb812988
commanderking <jek582@mail.harvard.edu>
2017-04-08 12:04:47 -0400

    change form based on submission media type

--- app/assets/javascripts/submit_story.js
+++ app/assets/javascripts/submit_story.js
@@ -1,7 +1,32 @@
 $(document).ready(function(){
   console.log($("#story_media_type").find(":selected").text());
 
-  $("#story_media_type").click(function(){
-    console.log($("#story_media_type").find(":selected").text());
+  $("#story_media_type").change(function(){
+    var storyType = $("#story_media_type").val();
+    var text = $(".text_unique_input");
+    var video = $(".video_unique_input");
+    var audio = $(".audio_unique_input");
+
+    switch(storyType) {
+      case "text":
+        audio.hide();
+        video.hide();
+        text.show();
+        break;
+      case "video":
+        audio.hide();
+        video.show();
+        text.hide();
+        break;
+      case "audio":
+        audio.show();
+        video.hide()
+        text.hide();
+        break;
+      default:
+        break;
+    }
+
+    console.log($("#story_media_type").val());
   })
 })
