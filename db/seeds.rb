2ce30d3ca2346b5d88fb333ea25964f5069a76e8
Allison King <allisonjuliaking@gmail.com>
2017-04-30 16:15:12 -0400

    add seed for tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tags_list = ["childhood",
+"politics",
+"military",
+"adulthood",
+"marriage",
+"immigration",
+"family",
+"kids",
+"KMT",
+"DPP",
+"martial law",
+"government",
+"parents",
+"food",
+"education",
+"identity",
+"Little League Baseball",
+"night markets",
+"Lunar New Year",
+"Taiwanese language",
+"aboriginal",
+"history",
+"culture",
+"art"]
+
+tags_list.each do |tag_name|
+  Tag.create(name: tag_name)
+end
