28d9add7e1bf89034a13537f4cf84ccf10cee42f
Allison King <allisonjuliaking@gmail.com>
2017-05-06 00:02:45 -0400

    adding soundcloud thumbnail

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'json'
+
 class Story < ApplicationRecord
   mount_uploader :thumbnail, ThumbnailUploader
   acts_as_taggable
@@ -20,12 +23,50 @@ class Story < ApplicationRecord
     order(:rank)
   end
 
-  def self.featured
-    where(is_featured: true).limit(6) || Story.by_date.first
+  def self.featured(count: nil)
+    #where(is_featured: true).limit(count) || Story.by_date.first
+    where(is_featured: true).order(:rank).limit(count)
   end
 
   def youtube_id
     YouTubeAddy.extract_video_id(self.video_link)[0,11]
   end
 
+  def soundcloud_thumb
+    get_soundcloud_thumb()
+  end
+
+  private
+  def get_soundcloud_thumb()
+    url = "http://soundcloud.com/oembed?format=js&url=" + self.video_link
+    puts url
+    uri = URI.parse(url)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    response = http.request(request)
+    body = response.body[1..-1].chomp(');')
+
+    result = JSON.parse(body)
+    return result['thumbnail_url']
+
+    result.each do |doc|
+      puts doc["id"] #reference properties like this
+      puts doc # =>  this is the result in object form
+      puts ""
+      puts ""
+    end
+  end
+
 end
+
+#  url = "http://soundcloud.com/oembed?format=js&url=" + self.video_link + "&iframe=true&callback=?"
+#  uri = URI(url)
+#  http = Net::HTTP.new(uri.host, uri.port)
+#  request = Net::HTTP::Get.new(uri.request_uri)
+#  response = http.request(request).body
+#  response = response[1..-1]
+#  puts response
+#  json_response = JSON.parse(response)
+#  return json_response['thumbnail_url']
